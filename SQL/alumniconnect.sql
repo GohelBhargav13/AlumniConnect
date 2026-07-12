73718c835122494ea1e79983974b6811f161794e
Gohel Bhargav <gohelbhargav401@gmail.com>
2026-07-12 17:19:35 +0530

    fix: update sql file

--- SQL/alumniconnect.sql
+++ SQL/alumniconnect.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jul 05, 2026 at 10:29 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Host: sql113.infinityfree.com
+-- Generation Time: Jul 12, 2026 at 07:45 AM
+-- Server version: 11.4.12-MariaDB
+-- PHP Version: 7.2.22
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `alumniconnect`
+-- Database: `if0_42342177_alumniconnect`
 --
 
 -- --------------------------------------------------------
@@ -41,7 +42,11 @@ CREATE TABLE `adminmaster` (
 --
 
 INSERT INTO `adminmaster` (`admin_id`, `admin_name`, `admin_email`, `password`, `created_At`, `update_at`) VALUES
-(1, 'Bhargav', 'gohelbhargav401@gmail.com', '$2y$10$qWAJ3w517JSQX8BLpFVHNuqOpkzQW/WoWgYdaJCzT/JonsXJ4gWVC', '2026-01-17 18:05:39', '2026-07-03 17:13:06');
+(1, 'Bhargav', 'gohelbhargav401@gmail.com', '$2y$10$6pd/eG3hfzg3ZB9y3CIsqOroShy4OxADnvVY2BKZrkUjgLjTJtRUi', '2026-01-17 18:05:39', '2026-07-06 08:45:28'),
+(2, 'Arya', 'aryal@gmail.com', '$2y$10$d11f2OFoIyZr25J9gUpxs.wPMTUph4hqj9rCgSgRhSFQnPBHdrhqu', '2026-07-06 05:34:23', '2026-07-06 12:35:36'),
+(3, 'Nayan', 'nayang@gmail.com', '$2y$10$aVc9SUG7BXnMo7qM3NcYfuioSuXmexJZYFW/Il90Km6llQBDY225i', '2026-07-06 05:34:23', '2026-07-06 12:36:33'),
+(4, 'Bhushan', 'bhushanj@gmail.com', '$2y$10$4gPwUXdWe28eLO/0jNoBN.K5JW8PWzgS1D.xmW5ygkjoWIQZw8gYa', '2026-07-06 05:34:50', '2026-07-06 12:37:41'),
+(5, 'Avinash', 'avinashc@gmail.com', '$2y$10$KqdKqep6A0sTq6dT4xuBi.3kl16CyMmOtaz7WoPPOxP6IWwLteTR6', '2026-07-12 04:12:49', '2026-07-12 11:25:55');
 
 -- --------------------------------------------------------
 
@@ -63,13 +68,6 @@ CREATE TABLE `alumni_profile` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `alumni_profile`
---
-
-INSERT INTO `alumni_profile` (`p_id`, `alumni_id`, `alumni_phone_no`, `alumni_address`, `alumni_batch`, `alumni_company`, `alumni_designation`, `alumni_city`, `alumni_linkedin_link`, `alumni_github_link`, `created_at`, `updated_at`) VALUES
-(14, 12, '01234567890', '', '2019', 'eSparkbiz', 'Software Engineer', 'porbandar', 'https://www.linkedin.com/in/bhargav-gohel-968919303', 'https://github.com/GohelBhargav13', '2026-07-05 08:17:15', '2026-07-05 08:17:15');
 
 -- --------------------------------------------------------
 
@@ -95,16 +93,12 @@ CREATE TABLE `alumni_student_master` (
 --
 
 INSERT INTO `alumni_student_master` (`alumni_id`, `alumni_name`, `email`, `passout_year`, `enrollment_no`, `branch`, `password_hash`, `is_registered`, `created_at`, `updated_at`) VALUES
-(11, 'Aarav Patel', 'aarav.patel@example.com', 2022, '22CE001', 'Computer Engineering', NULL, 0, '2026-07-05 08:10:08', '2026-07-05 08:10:08'),
-(12, 'Bhargav Gohel', 'bhargav.gohel@example.com', 2023, '23CE015', 'Computer Engineering', '$2y$10$T9qXl92xXVT0btPEnolQpeEFiJzOJSmX52ZmkoiOcuNO3YFG8xb5q', 1, '2026-07-05 08:10:08', '2026-07-05 08:16:29'),
-(13, 'Kinjal Shah', 'kinjal.shah@example.com', 2021, '21IT045', 'Information Technology', NULL, 0, '2026-07-05 08:10:08', '2026-07-05 08:10:08'),
-(14, 'Riya Mehta', 'riya.mehta@example.com', 2020, '20EC032', 'Electronics & Communication', NULL, 0, '2026-07-05 08:10:08', '2026-07-05 08:10:08'),
-(15, 'Dev Joshi', 'dev.joshi@example.com', 2024, '24ME010', 'Mechanical Engineering', NULL, 0, '2026-07-05 08:10:08', '2026-07-05 08:10:08'),
-(16, 'Nisha Desai', 'nisha.desai@example.com', 2019, '19CV078', 'Civil Engineering', NULL, 0, '2026-07-05 08:10:08', '2026-07-05 08:10:08'),
-(17, 'Yash Trivedi', 'yash.trivedi@example.com', 2018, '18EE056', 'Electrical Engineering', NULL, 0, '2026-07-05 08:10:08', '2026-07-05 08:10:08'),
-(18, 'Priya Modi', 'priya.modi@example.com', 2022, '22CE089', 'Computer Engineering', NULL, 0, '2026-07-05 08:10:08', '2026-07-05 08:10:08'),
-(19, 'Harsh Rana', 'harsh.rana@example.com', 2023, '23IT021', 'Information Technology', NULL, 0, '2026-07-05 08:10:08', '2026-07-05 08:10:08'),
-(20, 'Sneha Parmar', 'sneha.parmar@example.com', 2021, '21CE067', 'Computer Engineering', NULL, 0, '2026-07-05 08:10:08', '2026-07-05 08:10:08');
+(21, 'Avinash Choudhary', 'avinash.choudhary@example.com', 2026, '240163107001', 'Computer Engineering', NULL, 0, '2026-07-12 10:48:57', '2026-07-12 11:17:59'),
+(22, 'Bhargav Gohel', 'bhargav.gohel@example.com', 2026, '240163107002', 'Computer Engineering', NULL, 0, '2026-07-12 10:48:57', '2026-07-12 11:17:56'),
+(23, 'Kinjal Shah', 'kinjal.shah@example.com', 2026, '240163107003', 'Information Technology', NULL, 0, '2026-07-12 10:48:57', '2026-07-12 11:17:52'),
+(24, 'Nayan Gohel', 'nayan.gohel@example.com', 2026, '240163107004', 'Computer Engineering', '$2y$10$XdzyeUEKjonYAXL2c3T9A.KmWUalzHovEa0Cbv3xCwknuYXmruCIm', 1, '2026-07-12 10:48:57', '2026-07-12 11:18:21'),
+(25, 'Bhushan Joshi', 'bhushan.joshi@example.com', 2026, '240163107005', 'Computer Engineering', NULL, 0, '2026-07-12 10:48:57', '2026-07-12 11:18:03'),
+(26, 'Arya Lodhari', 'arya.lodhari@example.com', 2026, '240163107006', 'Information Technology', NULL, 0, '2026-07-12 10:48:57', '2026-07-12 11:18:05');
 
 -- --------------------------------------------------------
 
@@ -129,9 +123,7 @@ CREATE TABLE `announcement_master` (
 --
 
 INSERT INTO `announcement_master` (`anno_id`, `anno_title`, `anno_desc`, `anno_type`, `anno_show_until`, `anno_additional_links`, `created_by`, `created_at`, `updated_at`) VALUES
-(1, 'Sem Exam Schedule', 'This is the exam start schedule for mid exams.', 'Important', '2026-07-07', NULL, 1, '2026-07-04 20:18:07', '2026-07-04 20:18:07'),
-(2, 'Sem Exam Schedule - 2025', 'This is exam schedule for the 2025 mid sems', 'Normal', '2025-07-05', NULL, 1, '2026-07-04 20:41:42', '2026-07-04 20:43:13'),
-(3, 'Sports Event - 2024', 'The Sports event in the college for the students.', 'Normal', '2024-06-03', NULL, 1, '2026-07-04 20:42:20', '2026-07-04 20:42:20');
+(1, 'Sem Exam Schedule', 'This is the exam start schedule for mid exams.', 'Important', '2026-07-07', NULL, 1, '2026-07-04 20:18:07', '2026-07-04 20:18:07');
 
 -- --------------------------------------------------------
 
@@ -157,8 +149,7 @@ CREATE TABLE `event_master` (
 --
 
 INSERT INTO `event_master` (`event_id`, `event_name`, `event_desc`, `event_venue`, `event_date`, `event_time`, `event_additional_links`, `created_by`, `created_at`, `updated_at`) VALUES
-(2, 'Alumni Meet 2026', 'An Alumni meet for passout students', 'CE/IT department', '2026-07-05', '11:00:00', NULL, 1, '2026-07-04 11:16:08', '2026-07-04 11:16:08'),
-(3, 'Alumni Meet 2025', 'An alumni meet for the passout students', 'CE/IT department', '2025-07-05', '11:00:00', NULL, 1, '2026-07-04 12:04:25', '2026-07-04 12:05:21');
+(2, 'Alumni Meet 2026', 'An Alumnimeet for the 2026 passout year studentss.', 'CE/IT department', '2026-07-07', '10:00:00', NULL, 1, '2026-07-04 11:16:08', '2026-07-07 18:44:56');
 
 --
 -- Indexes for dumped tables
@@ -206,13 +197,13 @@ ALTER TABLE `event_master`
 -- AUTO_INCREMENT for table `alumni_profile`
 --
 ALTER TABLE `alumni_profile`
-  MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `alumni_student_master`
 --
 ALTER TABLE `alumni_student_master`
-  MODIFY `alumni_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `alumni_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `announcement_master`
